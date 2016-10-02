6f85cf0395b008e37df7b491eb4bea983d1eb811
Roger Thomas <roger.thomas87@gmail.com>
2016-10-02 14:28:27 +0100

    Read CSV

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python
 import pandas as pd
+
+
+def read_csv(file_name):
+    df = pd.read_csv(file_name)
+    df = df[["GO Report Date", 'GO District']]
+    df = df.rename(columns={"GO Report Date": "date", "GO District": "district"})
+    df['count'] = 1
+    return df
+
+
+def main(file_name):
+    df = read_csv(file_name)
+    print(df)
+
+
+if __name__ == "__main__":
+    main("crimes_small.csv")
