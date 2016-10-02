c332bc64dd5d49b2b5b6db3679609442794732e5
Roger Thomas <roger.thomas87@gmail.com>
2016-10-02 15:16:36 +0100

    Finish

--- main.py
+++ main.py
@@ -12,21 +12,6 @@ def read_csv(file_name):
 
 def convert_dates_to_months(df):
     df['month'] = df['date'].apply(lambda x: x.split('-')[1])
-    month_number_map = {
-        "Jan": 1,
-        "Feb": 2,
-        "Mar": 3,
-        "Apr": 4,
-        "May": 5,
-        "Jun": 6,
-        "Jul": 7,
-        "Aug": 8,
-        "Sep": 9,
-        "Oct": 10,
-        "Nov": 11,
-        "Dec": 12
-    }
-    df['month_number'] = df['month'].map(month_number_map)
     df = df.drop(['date'], axis=1)
     return df
 
@@ -46,7 +31,23 @@ def get_monthly_demand_index_by_district(df):
 
 
 def sort_df_by_month_demand_index_and_district(df):
-    df = df.sort_values(["month", "demand_index", "district"], ascending=False)
+    month_number_map = {
+        "Jan": 1,
+        "Feb": 2,
+        "Mar": 3,
+        "Apr": 4,
+        "May": 5,
+        "Jun": 6,
+        "Jul": 7,
+        "Aug": 8,
+        "Sep": 9,
+        "Oct": 10,
+        "Nov": 11,
+        "Dec": 12
+    }
+    df['month_number'] = df['month'].map(month_number_map)
+    df = df.sort_values(["month_number", "demand_index", "district"], ascending=True)
+    df = df.drop('month_number', axis=1)
     df = df.reset_index(drop=True)
     return df
 
@@ -61,5 +62,5 @@ def main(file_name):
 
 
 if __name__ == "__main__":
-    df = main("crimes_small.csv")
-    print(df)
+    df = main("crimes.csv")
+    df.to_csv('results.csv', index=False)
