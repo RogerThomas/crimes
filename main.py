25e82aa3c324f64fef5de5055ef0560fa33026bd
Roger Thomas <roger.thomas87@gmail.com>
2016-10-02 14:34:32 +0100

    Convert Date To Month

--- main.py
+++ main.py
@@ -10,8 +10,30 @@ def read_csv(file_name):
     return df
 
 
+def convert_dates_to_months(df):
+    df['month'] = df['date'].apply(lambda x: x.split('-')[1])
+    month_number_map = {
+        "Jan": 1,
+        "Feb": 2,
+        "Mar": 3,
+        "Apr": 4,
+        "May": 5,
+        "Jun": 6,
+        "Jul": 7,
+        "Aug": 8,
+        "Sep": 9,
+        "Oct": 10,
+        "Nov": 11,
+        "Dec": 12
+    }
+    df['month_number'] = df['month'].map(month_number_map)
+    df = df.drop(['date'], axis=1)
+    return df
+
+
 def main(file_name):
     df = read_csv(file_name)
+    df = convert_dates_to_months(df)
     print(df)
 
 
